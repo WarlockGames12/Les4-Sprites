14c8951cc58b8cb791871ed79a288694d66470ab
WarlockGames12 <fpweeyou@gmail.com>
2021-10-09 08:28:41 +0200

    sprites

---
+++ SpriteInheritance/Twister.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+public class Twister : Sprite
+{
+	public Twister(RectangleF screenPosition)
+	{
+        this.screenPosition = screenPosition;
+        imageFrame = new Rectangle(85, 190, 14, 14);
+        
+    }
+}
